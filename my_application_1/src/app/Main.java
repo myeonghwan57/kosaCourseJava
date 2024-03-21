232230ca29f3291bf30c6e635446740d6ee6d27c
COM <COM@DESKTOP-L2F7LSG>
2024-03-21 17:05:23 +0900

    kosaCourse First Commit

---
+++ my_application_1/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import pack1.A;
+import pack2.B;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 라이브러리에서 가져온 A를 사용해 보자.
+
+		A a = new A();
+		a.method();
+
+		B b = new B();
+		b.method();
+	}
+
+}
